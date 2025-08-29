ba83d1a4af0a6a1cfe756b65fb2220d46aefcb4e
Christina Kim <christina.kim@sjsu.edu>
2025-08-28 20:59:51 -0700

    format project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,35 @@
 export default function ProjectCard(props) {
-    const { name, description, img, link } = props
+    const { name, description, img, tools, link } = props
     // one project card
 
     return (
         <div>
-            <h1>{name}</h1>
-            <img 
-                src={img}
-                alt="Project image"
+            <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <img
+                    src={img}
+                    alt="Project image"
+                    className="relative mx-4 -mt-6 h-60 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r object-contain"
             />
-            <p>{description}</p>
-            {link !== null ? 
-                <a href={link} target="_blank" rel="noopener noreferrer">Source</a> : 
-                <p></p>
-            }
+            <div className="p-6">
+                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                {name}
+                </h5>
+                <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                {description}
+                </p>
+                <p className="mt-2 italic font-bold block font-sans leading-relaxed antialiased text-xs text-gray-400">
+                {tools}
+                </p>
+            </div>
+            <div className="p-6 pt-0">
+                {link !== null ? 
+                    <button data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        <a href={link} target="_blank" rel="noopener noreferrer">Source Code</a>
+                    </button> 
+                    : <p></p>
+                }
+            </div>
+            </div>
         </div>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,14 +4,22 @@ const projects = [
   {
     name: "SupportHub MCP Server",
     description: "MCP Server exposing Walmart's SupportHub API endpoints as tools for utilization through LLM chatbots",
-    img: "placeholder",
+    img: "resources/182288589.png",
+    tools: "TypeScript, ...",
+    link: null
+  },
+  {
+    name: "SupportHub Google ADK Agent",
+    description: "Google ADK chatbot built with the intentions of evaluating for efficiency and accuracy alongside partner intern's LangGraph agent.",
+    img: "resources/agent-development-kit.png",
+    tools: "Python, ...",
     link: null
   }
 ]
 
 export default function Projects() {
   return (
-    <div>
+    <div className="p-10 flex flex-col md:flex-row items-center justify-between min-h-screen">
       {
         projects.map((project, i) => 
           <ProjectCard 
@@ -19,6 +27,7 @@ export default function Projects() {
             name={project.name}
             description={project.description}
             img={project.img}
+            tools={project.tools}
             link={project.link}
           />
         )
